c283e0a138b6afb85f6a657a0bea4c8f17c0f42c
Michael Scholtz <michael.scholtz@outlook.com>
2015-06-14 19:38:29 +0200

    Made the code actually readable.

--- luabf.lua
+++ luabf.lua
@@ -4,46 +4,59 @@
 -- Licence: MIT
 --------------------------------------------------------
 
-return function(s)
-    local bf, d, p, sp = "[^><%+%-%.,%[%]]+", setmetatable({}, {__index = function() return 0 end}), 1, 1
-    s = s:gsub(bf, "")
+return function(code)
+    -- Brainfuck language consists of 8 instructions "><+-.,[]", everything else can be removed
+    local brainfuck = "[^><%+%-%.,%[%]]+"
 
-    while sp <= #s do
-        local c = s:sub(sp,sp)
+    -- Data tape consists of "infinite" amount of cells
+    local data = setmetatable({}, {__index = function() return 0 end})
 
-        if c == ">" then
-            p = p + 1
-        elseif c == "<" then
-            p = p - 1
-        elseif c == "+" then
-            d[p] = d[p] + 1
-        elseif c == "-" then
-            d[p] = d[p] - 1
-        elseif c == "." then
-            io.write(string.char(d[p]))
-        elseif c == "," then
-            local _c = io.read(1)
-            if _c ~= nil then d[p] = _c:byte() end
-        elseif c == "[" then
-            if d[p] == 0 then
+    -- Data pointer begins at position 1
+    local dataPointer = 1
+
+    -- Instruction pointer begins at position 1
+    local instructionPointer = 1
+
+    -- Remove everything from the code except for the 8 instructions defined above
+    code = code:gsub(brainfuck, "")
+
+    -- Implementation of the 8 instructions starts below
+    while instructionPointer <= #code do
+        local instruction = code:sub(instructionPointer,instructionPointer)
+
+        if instruction == ">" then
+            dataPointer = dataPointer + 1
+        elseif instruction == "<" then
+            dataPointer = dataPointer - 1
+        elseif instruction == "+" then
+            data[dataPointer] = data[dataPointer] + 1
+        elseif instruction == "-" then
+            data[dataPointer] = data[dataPointer] - 1
+        elseif instruction == "." then
+            io.write(string.char(data[dataPointer]))
+        elseif instruction == "," then
+            local character = io.read(1)
+            if character ~= nil then data[dataPointer] = character:byte() end
+        elseif instruction == "[" then
+            if data[dataPointer] == 0 then
                 local i = 1
                 while i ~= 0 do
-                    sp = sp + 1
-                    local _c = s:sub(sp, sp)
-                    if _c == "[" then i = i + 1 elseif _c == "]" then i = i - 1 end
+                    instructionPointer = instructionPointer + 1
+                    local _instruction = code:sub(instructionPointer, instructionPointer)
+                    if _instruction == "[" then i = i + 1 elseif _instruction == "]" then i = i - 1 end
                 end
             end
-        elseif c == "]" then
-            if d[p] ~= 0 then
+        elseif instruction == "]" then
+            if data[dataPointer] ~= 0 then
                 local i = 1
                 while i ~= 0 do
-                    sp = sp - 1
-                    local _c = s:sub(sp, sp)
-                    if _c == "]" then i = i + 1 elseif _c == "[" then i = i - 1 end
+                    instructionPointer = instructionPointer - 1
+                    local _instruction = code:sub(instructionPointer, instructionPointer)
+                    if _instruction == "]" then i = i + 1 elseif _instruction == "[" then i = i - 1 end
                 end
             end
         end
 
-        sp = sp + 1
+        instructionPointer = instructionPointer + 1
     end
 end
